24334942ecb3879330f2e46320456e7edd4e42ba
AlphaMALE888 <shivangtiwari819@gmail.com>
2025-11-23 13:28:03 +0530

    fix: render build with python 3.10 and stable deps

--- render-build.sh
+++ render-build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt --use-deprecated=legacy-resolver
+pip install -r requirements.txt
 
 uvicorn app.main:app --host 0.0.0.0 --port 10000
 
